28440e791e19972dd907d3b883f0f8ed26376904
Thimo Reumerman <thimo.reumerman@gmail.com>
2019-12-04 13:20:08 +0100

    Created MainFrame class and created JavaDoc for Application

--- JavaProject/src/wakkenspel/Application.java
+++ JavaProject/src/wakkenspel/Application.java
@@ -1,9 +1,19 @@
 package wakkenspel;
 
+/**
+ * In de Application class zorgen we dat de Mainframe aangaat.
+ * @author Thimo Reumerman 97050932
+ * @since 4-12-2019 
+ * @version 0.1
+ */
 public class Application {
 
+	/**
+	 * Roept de Mainframe aan
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		
+		MainFrame mainFrame = new MainFrame();
 	}
 
 }
---
+++ JavaProject/src/wakkenspel/MainFrame.java
@@ -0,0 +1,24 @@
+package wakkenspel;
+
+import javax.swing.JFrame;
+
+/**
+ * De MainFrame class maakt het JFrame aan en zorgt dat de panels er goed instaan
+ * @author Thimo Reumerman 97050932
+ * @since 4-12-2019 
+ * @version 0.1
+ */
+public class MainFrame extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	public MainFrame() {
+		
+	}
+	
+
+}
